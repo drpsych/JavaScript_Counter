dffafa16a248049de0289572c232651477456c31
drpsych <62787554+drpsych@users.noreply.github.com>
2023-03-08 18:11:36 -0500

    refactored and removed duplicate code

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 let add = document.querySelector('#add');
 let subtract = document.querySelector('#subtract');
 let reset = document.querySelector('#reset');
+let output = document.querySelector("#output");
 
-add.addEventListener("click", function() {
-    console.log('add');
-    let output = document.querySelector("#output");
-    let result = Number(output.innerText) + 1;
-    output.innerText = result;
-});
-
-subtract.addEventListener('click', function() {
-    console.log('subtract');
-    let output = document.querySelector("#output");
-    let result = Number(output.innerText) - 1;
+function calculate(number) {
+    let result = Number(output.innerText) + number;
     if (result < 0) {
         result = 0;
     };
     output.innerText = result;
+}
+
+add.addEventListener("click", function() {
+    console.log('add');
+    calculate(1);
+    // let output = document.querySelector("#output");
+    // let result = Number(output.innerText) + 1;
+    // output.innerText = result;
+});
+
+subtract.addEventListener('click', function() {
+    console.log('subtract');
+    calculate(-1);
+    // let output = document.querySelector("#output");
+    // let result = Number(output.innerText) - 1;
+    // if (result < 0) {
+    //     result = 0;
+    // };
+    // output.innerText = result;
 });
 
 reset.addEventListener('click', function() {
